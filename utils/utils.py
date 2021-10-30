2de975e27602e3d25512f1e8c86af0c75b354469
yakhyo <yakhyo9696@gmail.com>
2021-10-30 22:58:15 +0900

    Update utils.py

--- utils/utils.py
+++ utils/utils.py
@@ -5,10 +5,9 @@ class ConvBNAct(nn.Module):
     def __init__(self, in_channels, out_channels, kernel_size=1, stride=1, padding=0, groups=1, act=True, relu6=True):
         super(ConvBNAct, self).__init__()
         self.conv = nn.Sequential(
-            nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=kernel_size, stride=stride,
-                      padding=padding, groups=groups, bias=False),
+            nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=kernel_size, stride=stride, padding=padding, groups=groups, bias=False),
             nn.BatchNorm2d(num_features=out_channels),
-            nn.ReLU6(inplace=True) if act and relu6 else nn.ReLU(inplace=True))
+            nn.ReLU6() if act and relu6 else nn.ReLU())
 
     def forward(self, x):
         return self.conv(x)
@@ -18,10 +17,9 @@ class ConvBNSiLU(nn.Module):
     def __init__(self, in_channels, out_channels, kernel_size=1, stride=1, padding=0, groups=1):
         super(ConvBNSiLU, self).__init__()
         self.conv = nn.Sequential(
-            nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=kernel_size, stride=stride,
-                      padding=padding, groups=groups, bias=False),
+            nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=kernel_size, stride=stride, padding=padding, groups=groups, bias=False),
             nn.BatchNorm2d(num_features=out_channels),
-            nn.SiLU(inplace=True))
+            nn.SiLU())
 
     def forward(self, x):
         return self.conv(x)
@@ -31,7 +29,7 @@ class SEModule(nn.Module):
     def __init__(self, channels, ratio=12):
         super(SEModule, self).__init__()
         self.conv = nn.Sequential(nn.Conv2d(channels, channels // ratio, 1),
-                                  nn.ReLU(inplace=True),
+                                  nn.ReLU(),
                                   nn.BatchNorm2d(channels // ratio),
                                   nn.Conv2d(channels // ratio, channels, 1),
                                   nn.Sigmoid())
